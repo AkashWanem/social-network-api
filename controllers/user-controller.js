707cb4913d3602dfbb9a0818e1fd6470afdc0244
Akash Limbu <akashwanem@hotmail.com>
2022-05-08 14:05:29 +0100

    GET a single user by its _id and populated thought and friend data

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,10 +1,24 @@
-const { User, Thought } = require('../models');
+const { User, Thought } = require("../models");
 
 module.exports = {
-    // Get all users
-    getUsers(req, res) {
+  // Get all users
+  getUsers(req, res) {
     User.find()
       .then((users) => res.json(users))
       .catch((err) => res.status(500).json(err));
-    },
-}
+  },
+  // GET a single user by its _id and populated thought and friend data
+  getSingleUser(req, res) {
+    User.findOne({ _id: req.params.userId })
+      // populate users thoughts
+      .populate({path: 'thoughts', select: '-__v'})
+      // populate user friends
+      .populate({path: 'friends', select: '-__v'})
+      .then((user) =>
+        !user
+          ? res.status(404).json({ message: "No user with that ID" })
+          : res.json(user)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
+};
