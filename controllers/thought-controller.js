9cfae21245a39cb0b4b60a421e7b0387a72eef28
Akash Limbu <akashwanem@hotmail.com>
2022-05-08 20:35:58 +0100

    routes to get single thought by id

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -1,11 +1,21 @@
-const { User, Thought, Reaction } = require('../models');
+const { User, Thought, Reaction } = require("../models");
 
 module.exports = {
-    // get all thoughts
-    getAllThoughts(req, res){
-        Thought.find()
-        .then((users) => res.json(users))
-        .catch((err) => res.status(500).json(err));
-    },
-    
-}
+  // get all thoughts
+  getAllThoughts(req, res) {
+    Thought.find()
+      .then((users) => res.json(users))
+      .catch((err) => res.status(500).json(err));
+  },
+  // get a single thought by its _id
+  getThoughtById(req, res) {
+    Thought.findOne({ _id: req.params.id })
+      .then((user) =>
+        !user
+          ? res.status(404).json({ message: "No thought associated with that ID" })
+          : res.json(user)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
+  
+};
